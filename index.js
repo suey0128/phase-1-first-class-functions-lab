5cd0a2290e109f0d8810a7ce59c383fdfefcb12e
suey0128 <syui0128@gmail.com>
2021-05-20 12:23:34 -0700

    commit tree 69df33a28ee30c12beba9451c0d5d6977eac9dea

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+
+// const returnFirstTwoDrivers = function (x){
+//         return x.slice(0, 2);
+//     }
+// }
+
+// function returnFirstTwoDrivers() {
+//     const driverList = function (x) {
+//         return x.slice(0, 2);
+//     }
+// }
+function returnFirstTwoDrivers(x) {
+    return x.slice(0, 2);
+}
+
+function returnLastTwoDrivers(x) {
+    return x.slice(x.length-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+// function createFareMultiplier(y) {
+//     let inner = num =>  num * y;
+//     return inner;
+// }
+
+function createFareMultiplier(y) {
+    return num =>  num * y;
+}
+
+// let fareDoubler = (num) => num *2;
+
+function fareDoubler(num) {
+    const cb = createFareMultiplier(2);
+    return cb(num);
+}
+
+// let fareTripler = (num) => num *3;
+
+function fareTripler(num) {
+    const cb = createFareMultiplier(3);
+    return cb(num);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+}
